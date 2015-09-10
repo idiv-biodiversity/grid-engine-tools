da96aa58bb7798e02738167d53b42411444bdc46
Christian Krause <christian.krause@idiv.de>
2015-09-10 14:32:57 +0200

    adds qjdays - lists jobs by their remaining days
    motivation: useful to know for scheduling full system advance
    reservations for maintenance windows

---
+++ src/main/scala/qjdays.scala
@@ -0,0 +1,41 @@
+package grid.engine
+
+import sys.process._
+import util.{Try, Success, Failure}
+import xml._
+
+object qjdays extends App {
+  case class ADT(owner: String, runtime: Int)
+
+  object ADT {
+    def apply(owner: String, runtime: Option[Int]): Option[ADT] = runtime match {
+      case Some(runtime) => Some(ADT(owner, runtime))
+      case None          => None
+    }
+  }
+
+  def emptyStringOption(s: String) = if (s.isEmpty) None else Some(s)
+
+  def jobIDs = ("""qstat -s r -u *""" #| Seq("awk", "NR > 2 { print $1 }") #| """sort -u""").lineStream.par
+
+  def jobAnalysis(id: String): Option[ADT] = for {
+    qstatjxml <- Try(XML.loadString(s"""qstat -j $id -xml""".!!)) match {
+      case Success(value) => Some(value)
+      case Failure(error) =>
+        Console.err.println(s"""excluded $id due to: $error""")
+        None
+    }
+    owner <- emptyStringOption((qstatjxml \\ "JB_owner").text)
+    hardRequests = qstatjxml \\ "JB_hard_resource_list" \\ "element"
+    runtime <- hardRequests.find(el => (el \ "CE_name").text == "h_rt").map(el => (el \ "CE_stringval").text.toInt)
+  } yield ADT(owner, runtime)
+
+  jobIDs.map(jobAnalysis).seq.flatten.groupBy(_.runtime).mapValues {
+    _.map(_.owner).distinct
+  }.toSeq.sortBy(_._1) map {
+    case (seconds,owners) =>
+      val days = (seconds.toDouble / 60 / 60 / 24).round
+      val ownerList = owners.mkString(", ")
+      s"""h_rt = $seconds\tdays ~ $days\tusers = $ownerList"""
+  } foreach println
+}
