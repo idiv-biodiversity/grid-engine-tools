948ef4ce3e296f6d0b77322267b4f15f74a5926c
Christian Krause <christian.krause@idiv.de>
2015-09-10 13:58:05 +0200

    adds qquota-nice - a human-readable qquota
    - shows resource quota usage
    - insanely more readable than its original counterpart
      - not as exhaustive
      - but can be extended

---
+++ src/main/scala/qquota-nice.scala
@@ -0,0 +1,79 @@
+package grid.engine
+
+import sys.process._
+import xml.XML
+
+object qquotanice extends App {
+  val qquota = """qquota -xml -u *"""
+
+  val xml = XML.loadString(qquota.!!)
+
+  val timeSteps = List(1, 60, 60*60, 60*60*24).map(_.toLong)
+
+  def colorFor(limit: Double, value: Double): String = {
+    if (value >= limit)
+      Console.RED
+    else if (value >= 0.8 * limit)
+      Console.MAGENTA
+    else if (value >= 0.5 * limit)
+      Console.YELLOW
+    else
+      ""
+  }
+
+  trait ResourceEntry {
+    def user: String
+    def resource: String
+    def limit: Double
+    def value: Double
+  }
+
+  case class NumberEntry(user: String, resource: String, limit: Double, value: Double) extends ResourceEntry {
+    override def toString: String = {
+      val color = colorFor(limit.toDouble, value.toDouble)
+
+      f"""$color$user%-8s $resource%10s ${value.toLong}%15s ${limit.toLong}%15s${Console.RESET}"""
+    }
+  }
+
+  case class TimeEntry(user: String, resource: String, prettyLimit: String, prettyValue: String) extends ResourceEntry {
+    lazy val limit: Double = {
+      val tokens = prettyLimit.split(":").toList.map(_.toLong).reverse
+      tokens.zip(timeSteps).map(ab => ab._1 * ab._2).sum
+    }
+
+    lazy val value: Double = {
+      val tokens = prettyValue.split(":").toList.map(_.toLong).reverse
+      tokens.zip(timeSteps).map(ab => ab._1 * ab._2).sum
+    }
+
+    override def toString: String = {
+      val color = colorFor(limit, value)
+
+      f"""$color$user%-8s $resource%10s $prettyValue%15s $prettyLimit%15s${Console.RESET}"""
+    }
+  }
+
+  val entries: Seq[(String,ResourceEntry)] = for {
+    rule <- xml \\ "qquota_rule"
+    rulename = (rule \ "@name").text
+    user = (rule \ "users").text
+    limitxml = (rule \ "limit")
+    resource = (limitxml \ "@resource").text
+    limit = (limitxml \ "@limit").text
+    value = (limitxml \ "@value").text
+  } yield {
+    val entry = resource match {
+      case "h_rt" => new TimeEntry(user, resource, limit, value)
+      case "slots" => new NumberEntry(user, resource, limit.toDouble, value.toDouble)
+    }
+
+    (rulename,entry)
+  }
+
+  entries.groupBy(_._1).toSeq.sortBy(_._1) foreach { case (rulename,entries) =>
+    println(s"""$rulename:""")
+    entries.sortBy(- _._2.value) foreach { case (_,entry) => println(entry) }
+    println
+  }
+}
