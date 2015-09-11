45ab288503c3c8d110545b4ebcc5b7ddda5d7863
Christian Krause <christian.krause@idiv.de>
2015-09-11 14:22:01 +0200

    adds qcheckwaiters - shows opinionated waiting job info
    shows an opinionated excerpt of `qstat -j` for waiting jobs of a user
    
    motivation: be able to quickly determine why a user does not get
    resources

---
+++ src/main/bash/qcheckwaiters.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+[[ -n $1 ]] || {
+  echo "usage: $(basename $0 .sh) username"
+  exit 1
+}
+
+qstat -u $1 -s p |
+awk 'NR > 2 && $5 !~ /^(h|E)qw$/ { print $1 }' |
+parallel --tag 'qstat -j {} | grep -E "(^(hard|parallel|reserve|binding:))|exceeds limit|cannot run in PE"'
