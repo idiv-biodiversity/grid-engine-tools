d64af305c5baa41f5b0a829f099f3c78a1f6c1fa
Christian Krause <christian.krause@idiv.de>
2016-10-24 14:53:07 +0200

    reduces complexity by using awk more

--- src/main/bash/qlsunreservednodes.sh
+++ src/main/bash/qlsunreservednodes.sh
@@ -2,5 +2,5 @@
 
 command diff \
   <(qconf -sel) \
-  <(qrstat -u '*' | awk 'NR > 2 { print $1 }' | parallel qrstat -ar | grep ^granted | grep -oE '(node|idiv)[0-9]+' | sort) |
+  <(qrstat -u '*' | awk 'NR > 2 { system("qrstat -ar "$1) }' | grep ^granted | grep -oE '(node|idiv)[0-9]+' | sort) |
 awk '$1 == "<" { print $2 }'
