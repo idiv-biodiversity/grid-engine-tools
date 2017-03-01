62fb31a7ba7b2e268f9cc2f29070a323da83984b
Christian Krause <christian.krause@idiv.de>
2017-03-01 16:26:49 +0100

    allows qlsjobs to read input from stdin

--- src/main/bash/qlsjobs.sh
+++ src/main/bash/qlsjobs.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
 
-# TODO don't ignore task ids
+# TODO command line option to accept queue instances instead of nodes
+# TODO command line option to include task IDs
 
-[[ -n $1 ]] || {
-  echo "usage: $(basename $0 .sh) node [node...]" >&2
-  exit 1
+# ------------------------------------------------------------------------------
+# help / usage
+# ------------------------------------------------------------------------------
+
+function usage {
+cat << EOF
+usage: $(basename $0 .sh) [-?|--help] [node...]
+
+if no arguments are given, reads from stdin
+EOF
 }
 
-while [[ -n $1 ]] ; do
+if [[ "$1" == "-?" || "$1" == "--help" ]] ; then
+  usage
+  exit
+fi
+
+# ------------------------------------------------------------------------------
+# list jobs
+# ------------------------------------------------------------------------------
+
+function lsjobs {
   qhost -xml -j -h $1 | grep -oP "<job name='\K[0-9]+?(?=')"
-  shift
-done | sort -u
+}
+
+if [[ -n $1 ]] ; then
+  # iterate over command line arguments
+  while [[ -n $1 ]] ; do
+    lsjobs $1
+    shift
+  done
+else
+  # iterate over stdin lines
+  while read node ; do
+    [[ -n $node ]] &&
+    lsjobs $node
+  done < /dev/stdin
+fi | sort -u
