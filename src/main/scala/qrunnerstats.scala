5c382d444511377d76bb49bd718870b46a9ffe48
Christian Krause <christian.krause@idiv.de>
2019-01-14 16:58:50 +0100

    revamps qrunnerstats

--- src/main/scala/qrunnerstats.scala
+++ src/main/scala/qrunnerstats.scala
@@ -2,73 +2,89 @@ package grid.engine
 
 import cats.Eq
 import cats.instances.string._
-import scala.annotation.tailrec
 import scala.collection.immutable.TreeMap
 import scala.sys.process._
-import scala.xml._
 
-object qrunnerstats extends App {
-  def optLast[A](opts: String*)(convert: String => Option[A])(implicit args: List[String]): Option[A] = {
-    val longOpts = opts filter { _ startsWith "--" }
-    val longOptsRE = longOpts.mkString("^(","|",")=(.+)").r
+object qrunnerstats extends GETool {
 
-    @tailrec def optLastInternal(current: Option[A], args: List[String]): Option[A] = args match {
-      case Nil =>
-        current
+  def run(implicit conf: Conf): Unit = {
+    val qstat = "qstat -xml -ext -s r -u *"
+    val xml = XML.loadString(qstat.!!)
 
-      case opt :: arg :: tail if opts contains opt =>
-        optLastInternal(convert(arg), tail)
+    val userStats: TreeMap[String, Stats] =
+      (xml \\ "job_list").foldLeft(TreeMap[String, Stats]()) {
+        case (acc, job) =>
+          val project = (job \ "JB_project").text
 
-      case longOptsRE(_, arg) :: tail =>
-        optLastInternal(convert(arg), tail)
+          if (conf.project.fold(true)(_ === project)) {
+            val owner = (job \ "JB_owner").text
+            val slots = (job \ "slots").text.toInt
 
-      case _ :: tail =>
-        optLastInternal(current, tail)
+            val stats = Stats(1, slots)
+
+            val merged = acc.get(owner).foldLeft(stats)(_ + _)
+
+            acc.updated(owner, merged)
+          } else {
+            acc
+          }
+      }
+
+    val table = Table(Sized("user", "jobs", "cores"))
+
+    table.alignments(1) = Table.Alignment.Right
+    table.alignments(2) = Table.Alignment.Right
+
+    for ((user, Stats(jobs, slots)) ← userStats.toSeq.sortBy(- _._2.slots)) {
+      table.rows += Sized(user, s"$jobs", s"$slots")
     }
 
-    optLastInternal(None, args)
+    table.print()
   }
 
-  implicit val arguments = args.toList
-
-  object conf {
-    val project: Option[String] = optLast("-p", "--project") {
-      arg => Some(arg)
-    }
-  }
-
-  val cmd = "qstat -xml -ext -s r -u *"
-  val qstat = XML.loadString(cmd.!!)
-
-  val userStats: TreeMap[String, Stats] =
-    (qstat \\ "job_list").foldLeft(TreeMap[String, Stats]()) {
-      case (acc, job) =>
-        val project = (job \ "JB_project").text
-
-        if (conf.project.fold(true)(_ === project)) {
-          val owner = (job \ "JB_owner").text
-          val slots = (job \ "slots").text.toInt
-
-          val stats = Stats(1, slots)
-
-          val merged = acc.get(owner).foldLeft(stats)(_ + _)
-
-          acc.updated(owner, merged)
-        } else {
-          acc
-        }
-    }
-
-  userStats foreach {
-    case (user, stats) =>
-      println(f"""$user%-12s ${stats.jobs}%10d ${stats.slots}%10d""")
-  }
-
-  // -------------------------------------------------------------------------------------------------
-  // ADT
-  // -------------------------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
+  // data
+  // --------------------------------------------------------------------------
 
   final case class Stats(jobs: Int, slots: Int) {
     def +(other: Stats): Stats = Stats(jobs + other.jobs, slots + other.slots)
   }
+
+  // --------------------------------------------------------------------------
+  // configuration
+  // --------------------------------------------------------------------------
+
+  def app = "qrunnerstats"
+
+  final case class Conf (
+    debug: Boolean = false,
+    verbose: Boolean = false,
+    project: Option[String] = None,
+  ) extends Config
+
+  object Conf extends ConfCompanion {
+    def default = Conf()
+  }
+
+  def parser = new OptionParser[Conf](app) {
+    head(app, BuildInfo.version)
+
+    note("List running job statistics.\n")
+
+    note("FILTERS\n")
+
+    opt[String]('p', "project")
+      .valueName("<name>")
+      .action((name, c) => c.copy(project = Some(name)))
+      .text("filter by project")
+
+    note("\nOTHER OPTIONS\n")
+
+    help('?', "help").text("show this usage text")
+
+    version("version").text("show version")
+
+    note("")
+  }
+
 }
