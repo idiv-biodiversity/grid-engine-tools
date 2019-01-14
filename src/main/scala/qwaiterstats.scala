1ba42cc1e3a5c052d07f44f2dd60fe47b0552d9a
Christian Krause <christian.krause@idiv.de>
2019-01-14 16:58:39 +0100

    revamps qwaiterstats

--- src/main/scala/qwaiterstats.scala
+++ src/main/scala/qwaiterstats.scala
@@ -1,97 +1,96 @@
 package grid.engine
 
-import collection.immutable.TreeMap
-import sys.process._
-import util.control.Exception._
-import annotation.tailrec
+import cats.instances.string._
+import scala.collection.immutable.TreeMap
+import scala.sys.process._
 
-object qwaiterstats extends App with Signal {
+object qwaiterstats extends GETool {
 
-  exit on SIGPIPE
+  def run(implicit conf: Conf): Unit = {
+    val qstat = "qstat -xml -ext -s p -u *"
+    val xml = XML.loadString(qstat.!!)
 
-  def optLast[A](opts: String*)(convert: String => Option[A])(implicit args: List[String]): Option[A] = {
-    val longOpts = opts filter { _ startsWith "--" }
-    val longOptsRE = longOpts.mkString("^(","|",")=(.+)").r
+    val userStats: TreeMap[String, Stats] =
+      (xml \\ "job_list")
+        .flatMap(Stats.fromXML)
+        .foldLeft(TreeMap[String, Stats]()) {
+          case (acc, (user, stats)) =>
+            val merged = acc.get(user).foldLeft(stats)(_ + _)
+            acc.updated(user, merged)
+        }
 
-    @tailrec def optLastInternal(current: Option[A], args: List[String]): Option[A] = args match {
-      case Nil =>
-        current
+    val runners = (
+      "qstat -s r -u *" #|
+        Seq("awk", "NR > 2 { print $4 }") #|
+        Seq("sort", "-u")
+    ).lineStream.toArray
 
-      case opt :: arg :: tail if opts contains opt =>
-        optLastInternal(convert(arg), tail)
+    val table = Table(Sized("user", "jobs", "cores", "runners"))
 
-      case longOptsRE(_, arg) :: tail =>
-        optLastInternal(convert(arg), tail)
+    table.alignments(1) = Table.Alignment.Right
+    table.alignments(2) = Table.Alignment.Right
 
-      case _ :: tail =>
-        optLastInternal(current, tail)
+    for ((user, Stats(jobs, slots)) ← userStats.toSeq.sortBy(- _._2.slots)) {
+      // TODO when fansi in scala-cli-tools
+      // val u = if (runners contains user)
+      //   user
+      // else
+      //   s"""${Console.RED}${user}${Console.RESET}"""
+
+      val status = if (runners contains user) "" else "no"
+
+      table.rows += Sized(user, s"$jobs", s"$slots", status)
     }
 
-    optLastInternal(None, args)
+    table.print()
   }
 
-
-  implicit val arguments = args.toList
-
-  val projectArg: Option[String] = optLast("-p", "--project") {
-    arg => Some(arg)
-  }
-
-  val cmd = projectArg match {
-    case Some(project) =>
-      // scalastyle:off
-      "qstat -ext -s p -u *" #| Seq("awk","""NR > 2 && $6 == "%s" && $8 !~ /^(h|E)qw$/ { print $5, $15, $16 }""".format(project))
-      // scalastyle:on
-
-    case None =>
-      "qstat -s p -u *" #| Seq("awk","NR > 2 && $5 !~ /^(h|E)qw$/ { print $4, $8, $9 }")
-  }
-
-  val runners = ("qstat -s r -u *" #| Seq("awk","NR > 2 { print $4 }") #| Seq("sort","-u")).lineStream.toArray
-
-  val userStats: Map[String,Stats] = cmd.lineStream.foldLeft(TreeMap[String,Stats]()) { case (acc,line) =>
-    val tokens = line split " "
-    val user = tokens(0)
-    val slots = tokens(1).toInt
-
-    val tasks = catching(classOf[ArrayIndexOutOfBoundsException]) opt tokens(2) collect {
-      case Tasks(from, to, incr) =>
-        Range.count(from, to, incr, true)
-    } getOrElse 1
-
-    val stats = Stats(tasks, slots * tasks)
-
-    val merged = acc.get(user).foldLeft(stats)(_ + _)
-
-    acc.updated(user,merged)
-  }
-
-  userStats foreach {
-    case (user,stats) =>
-      val color = if (!runners.contains(user))
-        Console.RED
-      else
-        ""
-
-      println(f"""$color$user%-12s ${stats.jobs}%10d ${stats.slots}%10d${Console.RESET}""")
-  }
-
-  // -------------------------------------------------------------------------------------------------
-  // ADT
-  // -------------------------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
+  // data
+  // --------------------------------------------------------------------------
 
   final case class Stats(jobs: Int, slots: Int) {
     def +(other: Stats): Stats = Stats(jobs + other.jobs, slots + other.slots)
   }
 
-  // -------------------------------------------------------------------------------------------------
+  object Stats {
+    def fromXML(xml: Node)(implicit conf: Conf): Option[(String, Stats)] = {
+      val project = (xml \ "JB_project").text
+      val state = (xml \ "state").text
+
+      if (
+        conf.project.fold(true)(_ === project) &&
+          !state.startsWith("h") &&
+          !state.startsWith("E")
+      ) {
+        val owner = (xml \ "JB_owner").text
+        val slots = (xml \ "slots").text.toInt
+
+        val tasks = (xml \ "tasks").text match {
+          case Tasks(from, to, increment) ⇒
+            Range.count(from, to, increment, isInclusive = true)
+
+          case _ ⇒
+            1
+        }
+
+        val stats = Stats(tasks, slots * tasks)
+
+        Some(owner → stats)
+      } else {
+        None
+      }
+    }
+  }
+
+  // --------------------------------------------------------------------------
   // extractors
-  // -------------------------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
 
   object Tasks {
     val Regex = """(\d+)-(\d+):(\d+)""".r
 
-    def unapply(s: String): Option[(Int,Int,Int)] = s match {
+    def unapply(s: String): Option[(Int, Int, Int)] = s match {
       case Regex(from, to, incr) =>
         Some((from.toInt, to.toInt, incr.toInt))
 
@@ -99,4 +98,47 @@ object qwaiterstats extends App with Signal {
         None
     }
   }
+
+  // --------------------------------------------------------------------------
+  // configuration
+  // --------------------------------------------------------------------------
+
+  def app = "qwaiterstats"
+
+  final case class Conf (
+    debug: Boolean = false,
+    verbose: Boolean = false,
+    project: Option[String] = None,
+  ) extends Config
+
+  object Conf extends ConfCompanion {
+    def default = Conf()
+  }
+
+  def parser = new OptionParser[Conf](app) {
+    head(app, BuildInfo.version)
+
+    note("List waiting job statistics.\n")
+
+    note("FILTERS\n")
+
+    opt[String]('p', "project")
+      .valueName("<name>")
+      .action((name, c) => c.copy(project = Some(name)))
+      .text("filter by project")
+
+    note("\nOTHER OPTIONS\n")
+
+    opt[Unit]("debug")
+      .hidden()
+      .action((_, c) => c.copy(debug = true))
+      .text("show debug output")
+
+    help('?', "help").text("show this usage text")
+
+    version("version").text("show version")
+
+    note("")
+  }
+
 }
